291f7c35c7a5f71bfbe57cd75a65640fd79594d5
Rademero <47907410+Rademero@users.noreply.github.com>
2020-10-15 12:19:30 -0400

    Cubemap start
    This has the basics of the cube map, this is needed to corectly rotate sides

---
+++ Assets/Scripts/ReadCube.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadCube : MonoBehaviour
+{
+    public Transform tUp;
+    public Transform tDown;
+    public Transform tLeft;
+    public Transform tRight;
+    public Transform tFront;
+    public Transform tBack;
+
+    //private int layerMask = 1 << 8;
+    CubeState cubeState;
+    CubeMap cubeMap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeState = FindObjectOfType<CubeState>();
+        cubeMap = FindObjectOfType<CubeMap>();
+
+         List<GameObject> facesHit = new List<GameObject>();
+        Vector3 ray = tFront.transform.position;
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, tFront.right, out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(ray, tFront.right * hit.distance, Color.yellow);
+            facesHit.Add(hit.collider.gameObject);
+            print(hit.collider.gameObject.name);
+        }
+        else
+        {
+            Debug.DrawRay(ray, tFront.right * 1000, Color.green);
+        }
+        cubeState.front = facesHit;
+        cubeMap.Set();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
