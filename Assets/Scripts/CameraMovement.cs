490c49b73c9cc7068a40bc9ed3f85aea7aad3344
justmarsh <jlmarsha@mtu.edu>
2020-10-15 14:04:08 -0400

    Camera works fine and can zoom in and out.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,40 +1,104 @@
-﻿using System.Collections;
+﻿/* ===================================================================== */
+/* Attach this script to the camera that you are using for rotation      */
+/* around an object. The lookAt variable is the object you want to       */
+/* rotate around, and lookAT is set in the attributs of the camera.      */
+/* ===================================================================== */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
 public class CameraMovement : MonoBehaviour
 {
+    // Set outside of the script by the user.
     public Transform lookAt;
-    public Transform camTransform;
 
-    private Camera cam;
+    private Transform camTransform;
 
     private float distance = 10.0f;
-    private float currentX = 0.0f;
-    private float currentY = 0.0f;
-    
+    private float cameraSpeed = 100.0f;
+
+    private float cameraX;
+    private float cameraY;
+
+    // Variables used for zoomin
+    private float zoomSpeed = 0.15f;
+    private float minDistance = 5.0f;
+    private float maxDinstance = 20.0f;
+
     private void Start()
     {
+        // Since the script is attached to the camera transform is actually the camera's transform.
         camTransform = transform;
-        cam = Camera.main;
     }
 
     private void Update()
     {
-        currentX = currentX + Input.GetAxis("Horizontal");
-        currentY = currentY + Input.GetAxis("Vertical");
+        float angleDelta = cameraSpeed * Time.deltaTime;
+
+        // Rotate up
+        if (Input.GetKey("w"))
+        {
+            cameraX = cameraX + angleDelta;
+        }
+        // Rotate down
+        if (Input.GetKey("s"))
+        {
+            cameraX = cameraX - angleDelta;
+        }
+        // Rotate Left
+        if (Input.GetKey("a"))
+        {
+            cameraY = cameraY + angleDelta;
+        }
+        // Rotate right
+        if (Input.GetKey("d"))
+        {
+            cameraY = cameraY - angleDelta;
+        }
+
+        // Zoom in
+        if(Input.GetKey("q"))
+        {
+            zoom(true);
+        }
+        // Zoom out
+        if(Input.GetKey("e"))
+        {
+            zoom(false);
+        }
+
+        // Stops the camera from rotating over the top or bottom of the object.
+        //cameraX = Mathf.Clamp(cameraX, -90f, 90f);
+
+        // Allows the cammera to freely rotate around the lookAt object.
+        cameraX = Mathf.Repeat(cameraX, 360f);
+        // Allows the camera to fully rotate left and right around the object without any breaks
+        cameraY = Mathf.Repeat(cameraY, 360f);
+
+        Quaternion cameraRotation = Quaternion.AngleAxis(cameraY, Vector3.up) * Quaternion.AngleAxis(cameraX, Vector3.right);
+
+        Vector3 cameraPosition = lookAt.position + cameraRotation * Vector3.back * distance;
+
+        camTransform.position = cameraPosition;
+        camTransform.rotation = cameraRotation;
 
     }
 
-    private void LateUpdate()
+    // USed to zoom in and out from the lookAt object.
+    private void zoom(bool forward)
     {
-
-        Vector3 dir = new Vector3(0, 0, -distance);
-        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
-        camTransform.position = lookAt.position + rotation * dir;
-
-        //camTransfrom.LookAt(lookAt.position);
+        if(forward && distance > minDistance)
+        {
+            distance = distance - zoomSpeed;
+        }
+        else if(!forward && distance < maxDinstance)
+        {
+            distance = distance + zoomSpeed;
+        }
     }
-
 }
+
+
+
