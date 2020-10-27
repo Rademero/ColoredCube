4054d84667937fe04a3e9a6fef389b34e00da781
Patrick Philbin <patrick@philbin.net>
2020-10-27 13:50:55 -0400

    Functional pause menu in Model scene.

--- Assets/Scenes/Menu/MenuButtonScript.cs
+++ Assets/Scenes/Menu/MenuButtonScript.cs
@@ -1,10 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+
+//Script for handling menu behavior
+//Includes both Main Menu and Pause Menu functionality
 
 public class MenuButtonScript : MonoBehaviour
 {
     public GameObject MainMenu;
+    public GameObject[] pauseObjects;
+    public Timer timer;
+
+    void Start() {
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPaused");
+        HidePaused();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                timer.Stop();
+                ShowPaused();
+            }
+            else if (Time.timeScale == 0) {
+                Time.timeScale = 1;
+                timer.Play();
+                HidePaused();
+            }
+        }
+    }
 
     //Load the scene containing Timer functionality (only one currently implemented)
     public void TimedButton() {
@@ -26,4 +54,32 @@ public class MenuButtonScript : MonoBehaviour
         Application.Quit();
         UnityEditor.EditorApplication.isPlaying = false;    //This line quits the editor, and is only here for testing purposes.
     }
+
+    public void ResumeButton() {
+        Time.timeScale = 1;
+        timer.Play();
+        HidePaused();
+    }
+
+    public void RestartButton() { 
+        //Unimplemented
+    }
+
+    public void ToMainMenu() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+
+    //Hides the objects that should only show on pause
+    public void HidePaused() {
+        foreach (GameObject g in pauseObjects){
+            g.SetActive(false);
+        }
+    }
+
+    public void ShowPaused() {
+        foreach (GameObject g in pauseObjects){
+            g.SetActive(true);
+        }
+    }
+    
 }
